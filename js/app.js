29bdf7fb9df1e5eb82234e16cb701783feda2f76
Richard Weber <riche.weber@outlook.com>
2014-07-03 20:52:55 +0300

    Version 0.1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,57 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+
+
+(function(){
+
+    var app = angular.module('score-counterApp', []);
+
+    var players = [];
+
+    app.controller('FormController', function($scope){
+        $scope.player = {};
+
+        this.addPlayer = function(){
+            $scope.player.score = 0;
+            players.push($scope.player);
+            $scope.player = {};
+        };
+    });
+
+    app.controller('ListController', function($scope){
+        $scope.players = players;
+    });
+
+
+    app.controller('PanelController', function($scope){
+        this.activePanel = 1;
+        $scope.errorMessage = null;
+
+        this.setPanel = function(val){
+            if(players.length < 2){
+                $scope.errorMessage = "You need at least 2 players!";
+                return;
+            }
+            this.activePanel = val;
+
+        };
+
+        this.isActive = function(val){
+            return this.activePanel === val;
+        };
+    });
+
+    app.controller('GameController', function($scope){
+        this.players = players;
+
+        this.addScore = function(player){
+            player.score++;
+        }
+
+        this.moreThan2Players = function(){
+            return this.players.length > 2;
+        }
+    });
+})();
